dce8751a8a494ca8d3c542557d579482418b9c09
Peilin Ye <peilin.ye@bytedance.com>
2021-08-01 03:10:23 +0800

    tc/ingress: Introduce clsact egress mini-Qdisc option
    If the ingress Qdisc is in use, currently it is not possible to add
    another clsact egress mini-Qdisc to the same device without taking down
    the ingress Qdisc, since both sch_ingress and sch_clsact share the same
    handle (0xFFFF0000).
    
    To solve this issue, recently we added a new clsact egress mini-Qdisc
    option for sch_ingress in the kernel.  Support it in the q_ingress front
    end, and update the usage message accordingly.
    
    To turn on the egress mini-Qdisc:
    
        $ tc qdisc add dev eth0 ingress
        $ tc qdisc change dev eth0 ingress clsact-on
    
    Then users can add filters to the egress mini-Qdisc as usual:
    
        $ tc filter add dev eth0 egress protocol ip prio 10 \
    	    matchall action skbmod swap mac
    
    Deleting the ingress Qdisc removes the egress mini-Qdisc as well.  To
    remove egress mini-Qdisc only, use:
    
        $ tc qdisc change dev eth0 ingress clsact-off
    
    Finally, if the egress mini-Qdisc is enabled, the "show" command will
    print out a "clsact" flag to indicate it:
    
        $ tc qdisc show ingress
        qdisc ingress ffff: dev eth0 parent ffff:fff1 ----------------
        $ tc qdisc change dev eth0 ingress clsact-on
        $ tc qdisc show ingress
        qdisc ingress ffff: dev eth0 parent ffff:fff1 ---------------- clsact
    
    Signed-off-by: Peilin Ye <peilin.ye@bytedance.com>

--- tc/q_ingress.c
+++ tc/q_ingress.c
@@ -17,21 +17,45 @@
 
 static void explain(void)
 {
-	fprintf(stderr, "Usage: ... ingress\n");
+	fprintf(stderr,
+		"Usage: [ add | replace | link | delete ] ... ingress\n"
+		"       change ... ingress [ clsact-on | clsact-off ]\n"
+		" clsact-on\tenable clsact egress mini-Qdisc\n"
+		" clsact-off\tdelete clsact egress mini-Qdisc\n");
 }
 
 static int ingress_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 			     struct nlmsghdr *n, const char *dev)
 {
+	struct nla_bitfield32 flags = {
+		.selector = TC_INGRESS_SUPPORTED_FLAGS,
+	};
+	bool change = false;
+	struct rtattr *tail;
+
 	while (argc > 0) {
 		if (strcmp(*argv, "handle") == 0) {
 			NEXT_ARG();
-			argc--; argv++;
+		} else if (strcmp(*argv, "clsact-on") == 0) {
+			flags.value |= TC_INGRESS_CLSACT;
+			change = true;
+		} else if (strcmp(*argv, "clsact-off") == 0) {
+			flags.value &= ~TC_INGRESS_CLSACT;
+			change = true;
 		} else {
 			fprintf(stderr, "What is \"%s\"?\n", *argv);
 			explain();
 			return -1;
 		}
+
+		argc--;
+		argv++;
+	}
+
+	if (change) {
+		tail = addattr_nest(n, 1024, TCA_OPTIONS);
+		addattr_l(n, 1024, TCA_INGRESS_FLAGS, &flags, sizeof(flags));
+		addattr_nest_end(n, tail);
 	}
 
 	return 0;
@@ -40,7 +64,25 @@ static int ingress_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 static int ingress_print_opt(struct qdisc_util *qu, FILE *f,
 			     struct rtattr *opt)
 {
+	struct rtattr *tb[TCA_INGRESS_MAX + 1];
+	struct nla_bitfield32 *flags;
+
 	print_string(PRINT_FP, NULL, "---------------- ", NULL);
+
+	if (!opt)
+		return 0;
+
+	parse_rtattr_nested(tb, TCA_INGRESS_MAX, opt);
+
+	if (!tb[TCA_INGRESS_FLAGS])
+		return -1;
+	if (RTA_PAYLOAD(tb[TCA_INGRESS_FLAGS]) < sizeof(*flags))
+		return -1;
+
+	flags = RTA_DATA(tb[TCA_INGRESS_FLAGS]);
+	if (flags->value & TC_INGRESS_CLSACT)
+		print_string(PRINT_FP, NULL, "clsact", NULL);
+
 	return 0;
 }
 
